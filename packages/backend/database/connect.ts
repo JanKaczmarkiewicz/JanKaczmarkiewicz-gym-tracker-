856544ef38df6a1466112091fad6fe5d3b889055
JanKaczmarkiewicz <positiveKiddo@gmail.com>
2021-07-25 09:38:59 +0200

    feat: useFindAndModify: false

--- packages/backend/database/connect.ts
+++ packages/backend/database/connect.ts
@@ -4,4 +4,5 @@ export default () =>
   mongoose.connect(process.env.MONGODB_CONNECTION_STRING || "", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false,
   });
